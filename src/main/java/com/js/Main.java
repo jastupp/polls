df00981e9e5dc23abf8ab728072c2c74656e14a0
jas <john.stupples@gmail.com>
2018-08-20 19:48:16 +1000

    Added base files

---
+++ src/main/java/com/js/Main.java
@@ -0,0 +1,20 @@
+package com.js;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+
+    /**
+     * Main Method for application
+     *
+     * @param args -
+     */
+    public static void main(String[] args)
+    {
+        // run the application
+        SpringApplication.run(Main.class, args);
+    }
+}
